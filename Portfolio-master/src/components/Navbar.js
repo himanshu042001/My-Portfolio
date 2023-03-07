cb6d0afd1bd48482da4783224039fed32351d1a2
himanshu <mhshekhar04@gmail.com>
2023-03-07 09:25:29 +0530

    new update

--- Portfolio-master/src/components/Navbar.js
+++ Portfolio-master/src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/himanshu042001/My-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
